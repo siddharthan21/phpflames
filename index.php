b6fb5cb13bf39fc3e619cce8491676ab438d0c22
siddharthan21 <siddharthan44@gmail.com>
2023-07-16 19:22:46 +0530

    message

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   include_once 'lib/load.php';
 ?>
 <?php
-    ?>
+?>
 <!doctype html>
 <html lang="en" class="h-100">
 <header>
@@ -14,7 +14,6 @@
     <h1>Excited??</h1>
   <form method="post"    action="templete/display.php" >
     <div class="form-floating">
-
       <input type="text" name="bfname" class="form-control" id="floatingInput" placeholder="" required>
       <label for="floatingInput"  >Your Name</label>
     </div>
@@ -22,22 +21,14 @@
       <input type="text" name="gfname" class="form-control" id="floatingPassword" placeholder="" required>
       <label for="floatingPassword">Your Crush Name</label>
     </div>
-
-
     <input class="w-100 btn btn-lg btn-primary" id="btn" type="submit" value="Find" ></input>
     <p class="mt-5 mb-8 text-muted"></p>
   </form>
   <div>
-    
   </div>
 </main>
-
-
   <footer class="mt-auto text-white-50">
     </footer>
 </div>
-
-
-    
-  </body>
+</body>
 </html>
